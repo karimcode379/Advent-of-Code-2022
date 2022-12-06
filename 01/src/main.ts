da0e0a0d28c9b07c380dda4f5e102cd805af8f78
karimcode379 <karimcode379@gmail.com>
2022-12-06 14:14:47 +0100

    initial commit

---
+++ 01/src/main.ts
@@ -0,0 +1,31 @@
+import { readFileSync } from "node:fs";
+
+const input = readFileSync("./src/input.txt", { encoding: "utf-8" })
+	.trim()
+	.split("\n")
+	.map(Number);
+
+//part 01
+let sums: number[] = [];
+let sum: number = 0;
+
+input.forEach((n) => {
+	if (n === 0) {
+		sums.push(sum);
+		sum = 0;
+	} else {
+		sum += n;
+	}
+});
+
+let biggest: number = sums[0];
+sums.forEach((n) => {
+	if (n > biggest) biggest = n;
+});
+
+console.log(biggest);
+
+//part 02
+sums.sort();
+sums.reverse();
+console.log(sums[0] + sums[1] + sums[2]);
